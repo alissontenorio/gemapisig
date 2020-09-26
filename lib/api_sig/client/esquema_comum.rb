70e6faf49800d867f65598ccddc41891223a1df9
Alisson Tenório <alisson@nti.ufal.br>
2020-09-25 23:12:34 -0300

    Organização da lib

--- lib/api_sig/client/esquema_comum.rb
+++ lib/api_sig/client/esquema_comum.rb
@@ -1,6 +1,7 @@
 require "api_sig/client/global_constants"
 
-class Pessoa  
+class Pessoa
+  private_class_method :new   
   def self.find_by_id_pessoa(id)
     #byebug
     begin
@@ -10,7 +11,17 @@ class Pessoa
     end
   end
 
+  def self.find_by_cpf(cpf)
+    begin
+      JSON.parse(RestClient.get("#{API_URL}/pessoa/cpf/#{cpf}", {accept: :json}).body).first
+    rescue => ex
+      puts ex.response
+    end
+  end
   #/apisig/pessoa/cpf/:cpf
   #/apisig/pessoa/id_pessoa/:id_pessoa
   #/apisig/modalidade_educacao/id_modalidade_educacao/:id_modalidade_educacao
 end
+
+
+#new
