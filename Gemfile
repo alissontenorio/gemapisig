96304b65894ed6f410bee317c7c535f1eca3c7b1
Alisson Tenório <alisson@nti.ufal.br>
2020-09-25 22:36:59 -0300

    Pequena Mudança

--- Gemfile
+++ Gemfile
@@ -7,3 +7,8 @@ gem "rake", "~> 12.0"
 gem "rspec", "~> 3.0"
 
 gem "rest-client", "~> 2.1"
+
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+end
--- lib/api_sig/client.rb
+++ lib/api_sig/client.rb
@@ -5,11 +5,12 @@ class Error < StandardError; end
 API_URL = "http://127.0.0.1:3000/apisig/"
 
 class Pessoa
+  byebug
   def self.id_pessoa(id)
     begin
       JSON.parse(RestClient.get("#{API_URL}/pessoa/id_pessoa/#{id}", {accept: :json}).body).first
     rescue => ex
-      ex.response
+      ex
     end
   end
 end
