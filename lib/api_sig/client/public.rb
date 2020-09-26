d78eb53608cd54891ee153ab0b02c5513b13143d
Alisson Tenório <alisson@nti.ufal.br>
2020-09-26 13:10:03 -0300

    Desenvolvido esquema public

--- lib/api_sig/client/public.rb
+++ lib/api_sig/client/public.rb
@@ -10,18 +10,18 @@ end
 
 class Discente < Base
   def self.find_by_id_discente(id_discente)
-    json_request("#{PUBLIC_DISCENTE_ID_DISCENTE_URL}"/"#{id_discente}")
+    json_request("#{PUBLIC_DISCENTE_ID_DISCENTE_URL}/#{id_discente}")
   end
 
   def self.find_by_matricula(matricula)
-    json_request("#{PUBLIC_DISCENTE_MATRICULA_URL}"/"#{matricula}")
+    json_request("#{PUBLIC_DISCENTE_MATRICULA_URL}/#{matricula}")
   end
 
   def self.find_by_id_pessoa(id_pessoa)
-    json_request("#{PUBLIC_DISCENTE_ID_PESSOA_URL}"/"#{id_pessoa}")
+    json_request("#{PUBLIC_DISCENTE_ID_PESSOA_URL}/#{id_pessoa}")
   end
 
   def self.find_by_id_curso(id_curso)
-    json_request("#{PUBLIC_DISCENTE_ID_CURSO_URL}"/"#{id_curso}")
+    json_request("#{PUBLIC_DISCENTE_ID_CURSO_URL}/#{id_curso}")
   end
 end
