8cf29314dc6d3df65df350118dc3247c8b299e12
Alisson Tenório <alisson@nti.ufal.br>
2020-09-26 00:46:43 -0300

    Organização da lib

--- lib/api_sig/client/esquema_comum.rb
+++ lib/api_sig/client/esquema_comum.rb
@@ -2,27 +2,23 @@ require "api_sig/client/global_constants"
 
 class Pessoa
   private_class_method :new   
-  def self.find_by_id_pessoa(id)
-    #byebug
+
+  def self.json_request(url)
     begin
-      JSON.parse(RestClient.get("#{API_URL}/pessoa/id_pessoa/#{id}", {accept: :json}).body).first
+      JSON.parse(RestClient.get(url, {accept: :json}).body).first
     rescue => ex
-      puts ex.response
+      raise ex.response
     end
+  end 
+
+  def self.find_by_id_pessoa(id_pessoa)
+    self.json_request("#{PESSOA_ID_PESSOA_URL}/#{id_pessoa}")
   end
 
   def self.find_by_cpf(cpf)
     return "CPF precisa ser uma string" if cpf.class != String
-    begin
-      JSON.parse(RestClient.get("#{API_URL}/pessoa/cpf/#{cpf.to_s}", {accept: :json}).body).first
-    rescue => ex
-      puts ex.response
-    end
+    self.json_request(("#{PESSOA_CPF_URL}/#{cpf}")
   end
-  #/apisig/pessoa/cpf/:cpf
-  #/apisig/pessoa/id_pessoa/:id_pessoa
+
   #/apisig/modalidade_educacao/id_modalidade_educacao/:id_modalidade_educacao
 end
-
-
-#new
--- lib/api_sig/client/global_constants.rb
+++ lib/api_sig/client/global_constants.rb
@@ -1,1 +1,3 @@
-API_URL = "http://127.0.0.1:3000/apisig/"
+BASE_API_URL = "http://127.0.0.1:3000/apisig"
+PESSOA_ID_PESSOA_URL = "#{BASE_API_URL}/pessoa/id_pessoa"
+PESSOA_CPF_URL = "#{BASE_API_URL}/pessoa/cpf"
