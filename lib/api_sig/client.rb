fef05d221883554e6200f0a43403b5630bcfb678
Alisson Tenório <alissonal@gmail.com>
2020-09-27 13:54:48 -0300

    Mudanças na distribuição de diretórios e adicionado alguns metodos

--- lib/api_sig/client.rb
+++ lib/api_sig/client.rb
@@ -8,7 +8,7 @@
 #require "api_sig/client/stricto_sensu"
 #require "api_sig/client/tecnico"
 
-Dir["#{File.dirname(__FILE__)}/api_sig/client/**/*.rb"].each {|file| require file }
+Dir["#{File.dirname(__FILE__)}/api_sig/**/*.rb"].each {|file| require file }
 #Dir["/path/to/directory/*.rb"].each {|file| require file }
 
 class Error < StandardError; end
