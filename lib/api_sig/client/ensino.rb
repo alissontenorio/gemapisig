43d23415c861541bd6c1e7fcfdec9b46e1f46c11
Alisson Tenório <alissonal@gmail.com>
2020-09-26 22:55:56 -0300

    Desenvolvido esquema public

--- lib/api_sig/client/ensino.rb
+++ lib/api_sig/client/ensino.rb
@@ -1,5 +1,8 @@
 class Turma < Base
   #/apisig/turma/id_turma/:id_turma
+  def self.find_by_id_turma(id_turma)
+    json_request("#{ENSINO_TURMA_ID_TURMA_URL}/#{id_turma}")
+  end
 end
 
 class Docente < Base
