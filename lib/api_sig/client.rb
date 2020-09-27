ce246e22a3f2c771d929d5e782471aa1a7ce0df9
Alisson Tenório <alissonal@gmail.com>
2020-09-27 17:23:46 -0300

    Mudanças na distribuição de diretórios e adicionado alguns metodos

--- lib/api_sig/client.rb
+++ lib/api_sig/client.rb
@@ -1,4 +1,4 @@
-#require "api_sig/client/version"
+#require 'api_sig/client/version.rb'
 #require "api_sig/client/base"
 #require "api_sig/client/comum"
 #require "api_sig/client/rh"
@@ -9,10 +9,29 @@
 #require "api_sig/client/tecnico"
 
 #Dir["#{File.dirname(__FILE__)}/api_sig/**/*.rb"].each {|file| require file }
-byebug
-Dir[File.join(__dir__, '/api_sig/client', '*.rb')].each { |file| require file 
-puts file
-}
+#Dir["#{File.dirname(__FILE__)}/api_sig/client/*.rb"].each {|file| require file puts "alo"}
+#Dir[File.join(__dir__, '/api_sig/client', '*.rb')].each { |file| require file 
+#puts "alo"
+#}
 #Dir["/path/to/directory/*.rb"].each {|file| require file }
 
 class Error < StandardError; end
+
+#require "api_sig/client/version"
+#require "api_sig/client/base"
+
+#Dir[__dir__+ "/client/*.rb"].each { |file|
+#  require file
+#  puts "#{file}"
+#  puts Dir.pwd
+#}
+
+#Dir[__dir__+ "/client/**/*.rb"].each { |file|
+#  require file
+#  puts "#{file}"
+#  puts Dir.pwd
+#}
+
+
+require "api_sig/client/global_constants"
+puts BASE_API_URL
