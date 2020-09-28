6c16009ef38cab64c3658a1540868dad8634ef9d
Alisson Tenório <alissonal@gmail.com>
2020-09-28 02:16:12 -0300

    Alteração no componente_curricular_detalhes

--- lib/api_sig/client/json_request.rb
+++ lib/api_sig/client/json_request.rb
@@ -1,6 +1,6 @@
 def json_request(url)
   begin
-    JSON.parse(RestClient.get(url, {accept: :json}).body)
+    JSON.parse(RestClient.get(url, {accept: :json}).body) == {}
   rescue => ex
     puts ex.response
   end
