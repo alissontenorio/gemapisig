104bc61bda9b75a62c4f04d50246a642c3c8d607
Alisson Tenório <alissonal@gmail.com>
2020-09-28 14:20:34 -0300

    Colocado namespace

--- lib/api_sig/client/base.rb
+++ lib/api_sig/client/base.rb
@@ -1,6 +1,5 @@
 require "api_sig/client/global_constants"
 require "api_sig/client/json_request"
-
 module Sig
   class Base
     private_class_method :new
--- lib/api_sig/client/json_request.rb
+++ lib/api_sig/client/json_request.rb
@@ -6,4 +6,4 @@ module Sig
       puts ex.response
     end
   end
-end 
+end
