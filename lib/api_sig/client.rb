b49fc235eda74b7e97a3d3d3ab517ed0ad63e64f
Alisson Tenório <alissonal@gmail.com>
2020-09-28 02:06:37 -0300

    Alteração no componente_curricular_detalhes

--- lib/api_sig/client.rb
+++ lib/api_sig/client.rb
@@ -1,35 +1,7 @@
-#require 'api_sig/client/version.rb'
-#require "api_sig/client/base"
-#require "api_sig/client/comum"
-#require "api_sig/client/rh"
-#require "api_sig/client/public"
-#require "api_sig/client/ensino"
-#require "api_sig/client/lato_sensu"
-#require "api_sig/client/stricto_sensu"
-#require "api_sig/client/tecnico"
+# Carrega os rb da pasta client
+Dir[__dir__+ "/client/*.rb"].each { |file| require file }
 
-#Dir["#{File.dirname(__FILE__)}/api_sig/**/*.rb"].each {|file| require file }
-#Dir["#{File.dirname(__FILE__)}/api_sig/client/*.rb"].each {|file| require file puts "alo"}
-#Dir[File.join(__dir__, '/api_sig/client', '*.rb')].each { |file| require file 
-#puts "alo"
-#}
-#Dir["/path/to/directory/*.rb"].each {|file| require file }
+# Carrega os .rb das subpastas client
+Dir[__dir__+ "/client/**/*.rb"].each { |file| require file }
 
 class Error < StandardError; end
-
-#require "api_sig/client/version"
-#require "api_sig/client/base"
-
-Dir[__dir__+ "/client/*.rb"].each { |file|
-  require file
-#  puts "#{file}"
-#  puts Dir.pwd
-}
-
-Dir[__dir__+ "/client/**/*.rb"].each { |file|
-  require file
-#  puts "#{file}"
-#  puts Dir.pwd
-}
-
-#puts BASE_API_URL
