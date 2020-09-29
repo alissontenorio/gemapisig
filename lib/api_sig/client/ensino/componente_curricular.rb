d3445fd021047763d2e303c6a218603244c09933
Alisson Tenório <alissonal@gmail.com>
2020-09-29 16:52:58 -0300

    Alterações na estrutura

--- lib/api_sig/client/ensino/componente_curricular.rb
+++ lib/api_sig/client/ensino/componente_curricular.rb
@@ -3,15 +3,15 @@ module Sig
     # /apisig/componente_curricular/id_disciplina/:id_disciplina
     # /apisig/componente_curricular/codigo/:codigo
     # /apisig/componente_curricular/id_tipo_componente/:id_tipo_componente
-    def self.find_by_id_componente(id_disciplina)
+    def self.find_by_id_disciplina(id_disciplina)
       json_request("#{ENSINO_COMPONENTE_CURRICULAR_ID_DISCIPLINA_URL}/#{id_disciplina}")
     end  
 
-    def self.find_by_id_componente(codigo)
+    def self.find_by_codigo(codigo)
       json_request("#{ENSINO_COMPONENTE_CURRICULAR_CODIGO_URL}/#{codigo}")
     end  
 
-    def self.find_by_id_componente(id_componente)
+    def self.find_by_id_tipo_componente(id_tipo_componente)
       json_request("#{ENSINO_COMPONENTE_CURRICULAR_ID_TIPO_COMPONENTE_URL}/#{id_tipo_componente}")
     end  
   end
