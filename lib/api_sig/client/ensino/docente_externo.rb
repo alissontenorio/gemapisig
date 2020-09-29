d534aff8d673642bf8ae1b370a02edee836df306
Alisson Tenório <alissonal@gmail.com>
2020-09-29 17:33:12 -0300

    Alterações na estrutura

--- lib/api_sig/client/ensino/docente_externo.rb
+++ lib/api_sig/client/ensino/docente_externo.rb
@@ -1,7 +1,5 @@
 module Sig
   class DocenteExterno < Base
-    # /apisig/docente_externo/id_docente_externo/:id_docente_externo
-    # /apisig/docente_externo/id_pessoa/:id_pessoa
     def self.find_by_id_docente_externo(id_docente_externo)
       json_request("#{ENSINO_DOCENTE_EXTERNO_ID_DOCENTE_EXTERNO_URL}/#{id_docente_externo}")
     end
--- lib/api_sig/client/ensino/docente_turma.rb
+++ lib/api_sig/client/ensino/docente_turma.rb
@@ -1,4 +1,19 @@
 module Sig
   class DocenteTurma < Base
+    def self.find_by_id_docente(id_docente)
+      json_request("#{ENSINO_DOCENTE_TURMA_ID_DOCENTE_URL}/#{id_docente}")
+    end
+
+    def self.find_by_id_docente_turma(id_docente_turma)
+      json_request("#{ENSINO_DOCENTE_TURMA_ID_DOCENTE_TURMA_URL}/#{id_docente_turma}")
+    end
+
+    def self.find_by_id_docente_externo(id_docente_externo)
+      json_request("#{ENSINO_DOCENTE_TURMA_ID_DOCENTE_EXTERNO_URL}/#{id_docente_externo}")
+    end
+
+    def self.find_by_id_turma(id_turma)
+      json_request("#{ENSINO_DOCENTE_TURMA_ID_TURMA_URL}/#{id_turma}")
+    end
   end
 end
