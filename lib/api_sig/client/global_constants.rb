08f3b31e3098dd34e07d33a8fcf180780ccd4cff
Alisson Tenório <alissonal@gmail.com>
2020-09-30 14:32:13 -0300

    modificação no caminho da rota

--- lib/api_sig/client/global_constants.rb
+++ lib/api_sig/client/global_constants.rb
@@ -1,13 +1,13 @@
 module Sig
   BASE_API_URL = "http://127.0.0.1:3000/apisig"
     
-  COMUM_PESSOA_ID_PESSOA_URL = "#{BASE_API_URL}/pessoa/id_pessoa"
-  COMUM_PESSOA_CPF_URL = "#{BASE_API_URL}/pessoa/cpf"
-  COMUM_MODALIDADE_EDUCACAO_ID_MODALIDADE_EDUCACAO_URL = "#{BASE_API_URL}/modalidade_educacao/id_modalidade_educacao"
-  COMUM_MODALIDADE_EDUCACAO_GET_ALL_URL = "#{BASE_API_URL}/modalidade_educacao"
+  COMUM_PESSOA_ID_PESSOA_URL = "#{BASE_API_URL}/comum/pessoa/id_pessoa"
+  COMUM_PESSOA_CPF_URL = "#{BASE_API_URL}/comum/pessoa/cpf"
+  COMUM_MODALIDADE_EDUCACAO_ID_MODALIDADE_EDUCACAO_URL = "#{BASE_API_URL}/comum/modalidade_educacao/id_modalidade_educacao"
+  COMUM_MODALIDADE_EDUCACAO_GET_ALL_URL = "#{BASE_API_URL}/comum/modalidade_educacao"
 
-  RH_SERVIDOR_ID_SERVIDOR_URL = "#{BASE_API_URL}/servidor/id_servidor"
-  RH_SERVIDOR_ID_PESSOA_URL = "#{BASE_API_URL}/servidor/id_pessoa"
+  RH_SERVIDOR_ID_SERVIDOR_URL = "#{BASE_API_URL}/rh/servidor/id_servidor"
+  RH_SERVIDOR_ID_PESSOA_URL = "#{BASE_API_URL}/rh/servidor/id_pessoa"
 
   PUBLIC_CURSO_ID_CURSO_URL = "#{BASE_API_URL}/curso/id_curso"
   PUBLIC_CURSO_GET_ALL_URL = "#{BASE_API_URL}/curso"
