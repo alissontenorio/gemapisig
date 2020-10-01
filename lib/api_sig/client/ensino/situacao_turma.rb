a0d69c6abae4394d41042996f29a345257ea755b
Alisson Tenório <alissonal@gmail.com>
2020-10-01 17:03:56 -0300

    Mudanças em situacao turma

--- lib/api_sig/client/ensino/situacao_turma.rb
+++ lib/api_sig/client/ensino/situacao_turma.rb
@@ -1,8 +1,11 @@
 module Sig
   class SituacaoTurma < Base
-    # /apisig/situacao_turma/id_situacao_turma/:id_situacao_turma
     def self.find_by_id_situacao_turma(id_situacao_turma)
       json_request("#{ENSINO_SITUACAO_TURMA_ID_SITUACAO_TURMA_URL}/#{id_situacao_turma}")
+    end
+
+    def self.all
+      json_request("#{ENSINO_SITUACAO_TURMA_GET_ALL_URL}")
     end
   end
 end
