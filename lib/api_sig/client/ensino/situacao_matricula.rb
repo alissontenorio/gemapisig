44873c7aef6ba057e0ef17c7dd0a22eaa11ab855
Alisson Tenório <alissonal@gmail.com>
2020-10-02 12:12:50 -0300

    Adicionado Tipo componente curricular

--- lib/api_sig/client/ensino/situacao_matricula.rb
+++ lib/api_sig/client/ensino/situacao_matricula.rb
@@ -1,5 +1,12 @@
 module Sig
   class SituacaoMatricula < Base
     # /apisig/situacao_matricula/id_situacao_matricula/:id_situacao_matricula
+    def self.find_by_id_situacao_matricula(id_situacao_matricula)
+      json_request("#{ENSINO_SITUACAO_MATRICULA_ID_SITUACAO_MATRICULA_URL}/#{id_situacao_matricula}")
+    end
+
+    def self.all
+      json_request("#{ENSINO_SITUACAO_MATRICULA_GET_ALL_URL}")
+    end
   end
 end
--- lib/api_sig/client/ensino/tipo_componente_curricular.rb
+++ lib/api_sig/client/ensino/tipo_componente_curricular.rb
@@ -1,6 +1,5 @@
 module Sig
   class TipoComponenteCurricular < Base
-    # /apisig/tipo_componente_curricular/id_tipo_disciplina/:id_tipo_disciplina
     def self.find_by_id_tipo_disciplina(id_tipo_disciplina)
       json_request("#{ENSINO_TIPO_COMPONENTE_CURRICULAR_ID_TIPO_DISCIPLINA_URL}/#{id_tipo_disciplina}")
     end
