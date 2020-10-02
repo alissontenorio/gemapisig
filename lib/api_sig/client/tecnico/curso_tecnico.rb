7da344a8e2d3ad0a4a51212ab493ea4ecb084744
Alisson Tenório <alissonal@gmail.com>
2020-10-02 15:46:57 -0300

    Adicionado Tipo componente curricular

--- lib/api_sig/client/tecnico/curso_tecnico.rb
+++ lib/api_sig/client/tecnico/curso_tecnico.rb
@@ -1,8 +1,5 @@
 module Sig
   class CursoTecnico < Base
-    # /apisig/curso_tecnico/codigo_inep/:codigo_inep
-    # /apisig/curso_tecnico/id_curso/:id_curso
-    # /apisig/curso_tecnico/id_modalidade_curso_tecnico/:id_modalidade_curso_tecnico
     def self.find_by_codigo_inep(codigo_inep)
       json_request("#{TECNICO_CURSO_TECNICO_CODIGO_INEP_URL}/#{codigo_inep}")
     end
--- lib/api_sig/client/tecnico/discente_tecnico.rb
+++ lib/api_sig/client/tecnico/discente_tecnico.rb
@@ -3,9 +3,11 @@ module Sig
     # /apisig/discente_tecnico/id_discente/:id_discente
     # /apisig/discente_tecnico/id_turma_entrada/:id_turma_entrada
     def self.find_by_id_discente(id_discente)
+      json_request("#{TECNICO_DISCENTE_TECNICO_ID_DISCENTE_URL}/#{id_discente}")
     end
 
     def self.find_by_id_turma_entrada(id_turma_entrada)
+      json_request("#{TECNICO_DISCENTE_TECNICO_ID_TURMA_ENTRADA_URL}/#{id_turma_entrada}")
     end
   end
 end
